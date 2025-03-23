ae0cbcf97924bd0d3f75f0d8f410d9ed49799a54
Sergey <konsaltpns@gmail.com>
2025-03-23 22:51:02 +0200

    update vercel deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,17 @@
+const Header = () => {
+  return (
+    <header>
+      <p>Text Header!</p>
+   </header> 
+  )
+}
+
 const App = () => {
-  return <div>Hello world!</div>
+  return (
+    <>
+      <div>Hello world!</div>
+      <Header />
+</>
+  )
 }
 export default App;
